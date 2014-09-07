dc63a03e03ba0d969016bba8b6518f8987c53916
Michal Brašna <michal.brasna@gmail.com>
2014-09-07 10:59:33 +0200

    Finish nyancat

--- build/component.js
+++ build/component.js
@@ -243,6 +243,41 @@ var i={};i[g]=(f=="show"?b=="pos"?"+=":"-=":b=="pos"?"-=":"+=")+e;a.animate(i,{q
 (function(e){e.effects.transfer=function(a){return this.queue(function(){var b=e(this),c=e(a.options.to),d=c.offset();c={top:d.top,left:d.left,height:c.innerHeight(),width:c.innerWidth()};d=b.offset();var f=e('<div class="ui-effects-transfer"></div>').appendTo(document.body).addClass(a.options.className).css({top:d.top,left:d.left,height:b.innerHeight(),width:b.innerWidth(),position:"absolute"}).animate(c,a.duration,a.options.easing,function(){f.remove();a.callback&&a.callback.apply(b[0],arguments);
 b.dequeue()})})}})(jQuery);
 ;
-$(document).ready(function() {
-	$(".rainbow").switchClass("rainbow", "rainbow-loaded", 10000);
+var SBKSNyancat = {},
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+SBKSNyancat.WebComponent = (function(_super, $) {
+  __extends(WebComponent, _super);
+
+  function WebComponent() {
+    WebComponent.__super__.constructor.apply(this, arguments);
+  }
+
+  WebComponent.prototype.createdCallback = function() {
+    this.template = '<div class="wrapper">\
+      <div class="rainbow"></div>\
+      <div class="nyan-cat">\
+        <img src="../nyan-cat.gif" alt="nyan-cat" />\
+      </div>\
+    </div>';
+  };
+
+  WebComponent.prototype.attachedCallback = function() {
+    var time = +this.getAttribute('time');
+    this.innerHTML = this.template;
+
+    $('.rainbow', this).switchClass("rainbow", "rainbow-loaded", time ? time : 10000);
+  };
+
+  WebComponent.prototype.detachedCallback = function() {
+  };
+
+  return WebComponent;
+
+})(HTMLElement, jQuery);
+
+SBKSNyancat = document.registerElement('sbks-nyancat', {
+  prototype: SBKSNyancat.WebComponent.prototype
 });
--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,38 @@
-$(document).ready(function() {
-	$(".rainbow").switchClass("rainbow", "rainbow-loaded", 10000);
+var SBKSNyancat = {},
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+SBKSNyancat.WebComponent = (function(_super, $) {
+  __extends(WebComponent, _super);
+
+  function WebComponent() {
+    WebComponent.__super__.constructor.apply(this, arguments);
+  }
+
+  WebComponent.prototype.createdCallback = function() {
+    this.template = '<div class="wrapper">\
+      <div class="rainbow"></div>\
+      <div class="nyan-cat">\
+        <img src="../nyan-cat.gif" alt="nyan-cat" />\
+      </div>\
+    </div>';
+  };
+
+  WebComponent.prototype.attachedCallback = function() {
+    var time = +this.getAttribute('time');
+    this.innerHTML = this.template;
+
+    $('.rainbow', this).switchClass("rainbow", "rainbow-loaded", time ? time : 10000);
+  };
+
+  WebComponent.prototype.detachedCallback = function() {
+  };
+
+  return WebComponent;
+
+})(HTMLElement, jQuery);
+
+SBKSNyancat = document.registerElement('sbks-nyancat', {
+  prototype: SBKSNyancat.WebComponent.prototype
 });
